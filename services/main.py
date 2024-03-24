efc094b75e90d525e5ea8fa3719c4c51c4a98dbe
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2024-03-24 20:35:29 +0700

    add auto reload service

--- services/main.py
+++ services/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True # auto reload when change the file
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("home.html")
